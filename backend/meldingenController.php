42959caf0f86544b8c78f4db15fdf1a8719182e6
jbgamingnl <jake@jbgaming.nl>
2025-02-13 14:01:41 +0100

    finished tickets added to the database

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -3,7 +3,27 @@
 //Variabelen vullen
 $attractie = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit']; 
+if
+    (!is_numeric(value: $capaciteit))
+    $errors = "That is not a number";
+    echo $errors; 
+    die;
+
 $melder = $_POST['melder'];
+if(!is_string($melder))
+$errors = "that is not text";
+die
+$Type   = $_POST['Type'];
+if(isset($_POST['prioriteit']))
+{
+    $prioriteit = 1;
+}
+else 
+{
+    $prioriteit = 0;
+}
+$overige_info = $_POST['overige_info'];
+
 
 echo $attractie . " / " . $capaciteit . " / " . $melder;
 
@@ -11,7 +31,17 @@ echo $attractie . " / " . $capaciteit . " / " . $melder;
 require_once 'conn.php';
 
 //2. Query
-
+$query = "INSERT INTO `meldingen` (attractie, capaciteit, melder, Type, prioriteit, overige_info ) VALUES (:attractie , :capaciteit, :melder , :Type ,  :prioriteit, :overige_info)";
 //3. Prepare
-
+$statement = $conn->prepare($query);
 //4. Execute
+$statement->execute([
+    ":attractie" => $attractie,
+    ":capaciteit" => $capaciteit,
+    ":melder" => $melder,
+    ":Type" => $Type, 
+    ":prioriteit" => $prioriteit,
+    ":overige_info" => $overige_info,
+]);
+
+header("location: ../meldingen/index.php?msg=melding opgeslagen");
--- meldingen/create.php
+++ meldingen/create.php
@@ -12,30 +12,50 @@
 
     <div class="container">
         <h1>Nieuwe melding</h1>
-
         <form action="../backend/meldingenController.php" method="POST">
-        
-            <div class="form-group">
-                <label for="attractie">Naam attractie:</label>
-                <input type="text" name="attractie" id="attractie" class="form-input">
-            </div>
+
+
+        <div class="form-group">
+            <label for="attractie">Naam attractie:</label>
+            <input type="text" name="attractie" id="attractie" class="form-input">
+        </div>
+        <div class="form-group">
+
+            <!-- hier komt een dropdown -->
             <div class="form-group">
                 <label for="type">Type</label>
-                <!-- hier komt een dropdown -->
-            </div>
-            <div class="form-group">
-                <label for="capaciteit">Capaciteit p/uur:</label>
-                <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
-            </div>
-            <div class="form-group">
-                <label for="melder">Naam melder:</label>
-                <input type="text" name="melder" id="melder" class="form-input">
-            </div>
-            
-            <input type="submit" value="Verstuur melding">
+                <select name="Type" id="Type">
+                    <option value="">kies een type</option>
+                    <option value="achtbaan">achtbaan</option>
+                    <option value="horeca">horeca</option>
+                    <option value="show">show</option>
+                    <option value="water">water</option>
+                    <option value="sanitair">sanitair</option>
+                </select>
 
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="capaciteit">Capaciteit p/uur:</label>
+            <input type="number" min="0" name="capaciteit" id="capaciteit" class="form-input">
+        </div>
+        <div class="form-group">
+            <label for="prioriteit">hoge prioriteit</label>
+            <input type="checkbox" id="prioriteit" name="prioriteit" value="1">
+        </div>
+        <div class="form-group">
+            <label for="melder">Naam melder:</label>
+            <input type="text" name="melder" id="melder" class="form-input">
+        </div>
+        <div class="form-group">
+            <label for="overig">overige informatie</label>
+            <textarea name="overige_info" id="overige_info" class="form-input" rows="4"></textarea>
+        </div>
+        
+            <input type="submit" value="Verstuur melding">
+        
         </form>
-    </div>  
+    </div>
 
 </body>
 
